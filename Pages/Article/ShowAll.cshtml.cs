3d8eba4871351ab9456f0fdff7ab39122556fb40
JulioJu <juanes0890@gmail.com>
2018-12-03 21:51:32 +0100

    Add Article/ShowAll (very factorized and easy to create thanks the last commit)

---
+++ Pages/Article/ShowAll.cshtml.cs
@@ -0,0 +1,16 @@
+namespace Videotheque.Pages.ArticlePage
+{
+  using Videotheque.Data;
+  using Videotheque.Pages.Abstract;
+
+  public class ShowAll : ShowAllAbstract<Article>
+  {
+    public ShowAll(AppDbContext db)
+      : base(db, db.Articles)
+    {
+    }
+
+  }
+}
+
+// vim:sw=2:ts=2:et:fileformat=dos
